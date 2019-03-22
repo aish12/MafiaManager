0181d400642b95c1d44ce467a585eeeb3ffb0331
Tesia Wu <tesiawu@tesias-mbp.attlocal.net>
2019-03-21 22:13:23 -0500

    added the loading page after login and all the segues, need to add constraints and the actual backend code

---
+++ MafiaManager/LoadingWelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoadingWelcomeViewController.swift
+//  MafiaManager
+//
+//  Created by Tesia Wu on 3/21/19.
+//  Copyright © 2019 Aishwarya Shashidhar. All rights reserved.
+//
+
+import UIKit
+
+class LoadingWelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
