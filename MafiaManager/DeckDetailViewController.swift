72332e1ba17fcb69b531989ee65ef10839ca9a2c
Robert Stigler <rstig@panj.cs.utexas.edu>
2019-03-26 22:17:23 -0500

    added action sheet for adding cards

--- MafiaManager/DeckDetailViewController.swift
+++ MafiaManager/DeckDetailViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class DeckDetailViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addCardButtonPressed(_ sender: Any) {
+        let optionMenu = UIAlertController(title: nil, message: "Choose Option", preferredStyle: .actionSheet)
+        
+        let createAction = UIAlertAction(title: "Create a New Card", style: .default) { (action) in
+            self.performSegue(withIdentifier: "fromDetailToNewCardSegue", sender: nil)
+        }
+        
+        let copyAction = UIAlertAction(title: "Copy Existing Card", style: .default) { (action) in
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        optionMenu.addAction(createAction)
+        optionMenu.addAction(copyAction)
+        optionMenu.addAction(cancelAction)
+        
+        // 5
+        self.present(optionMenu, animated: true, completion: nil)
+    }
     @IBAction func displayActionSheet(_ sender: Any) {
         // 1
         let optionMenu = UIAlertController(title: nil, message: "Choose Option", preferredStyle: .actionSheet)
