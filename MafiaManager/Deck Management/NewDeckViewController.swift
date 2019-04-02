bd97c1cdbb2093ddb964dd0fb92b12868be268b3
Robert Stigler <rstig@nga.cs.utexas.edu>
2019-04-02 16:35:34 -0500

    Added image picker for new deck

--- MafiaManager/Deck Management/NewDeckViewController.swift
+++ MafiaManager/Deck Management/NewDeckViewController.swift
@@ -8,18 +8,35 @@
 //  Manages the view for creating a new deck
 import UIKit
 
-class NewDeckViewController: UIViewController {
-
+class NewDeckViewController: UIViewController, ImagePickerDelegate {
     @IBOutlet weak var deckNameTextView: UITextView!
     @IBOutlet weak var deckDetailTextView: UITextView!
+    @IBOutlet weak var deckImagePickerButton: UIButton!
+    
+    var imagePicker: ImagePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+    }
+    
+    
+    @IBAction func imagePickerButtonPressed(_ sender: UIButton) {
+        self.imagePicker.present(from: sender)
+    }
+    
+    func didSelect(image: UIImage?) {
+        self.deckImagePickerButton.setImage(image, for: .normal)
     }
     
     // When the done button is pressed, the "new deck" view is popped
     // So the user returns to the Decks view
     @IBAction func doneButtonPressed(_ sender: Any) {
+        if let newName = deckNameTextView.text,
+            let newDescription = deckDetailTextView.text {
+            
+        }
         navigationController?.popViewController(animated: true)
     }
     /*
