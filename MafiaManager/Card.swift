8d74b2a38fcac54d56b5e52ba04f1fe296e3691c
Robert Stigler <rstig@nga.cs.utexas.edu>
2019-04-02 15:56:47 -0500

    skeletons of card and deck objects created

---
+++ MafiaManager/Card.swift
@@ -0,0 +1,15 @@
+	//
+//  Card.swift
+//  MafiaManager
+//
+//  Created by Robert Stigler on 4/2/19.
+//  Copyright © 2019 Aishwarya Shashidhar. All rights reserved.
+//
+//  Represents a user's custom card. Is comprised of a title, description,
+//  and picture
+
+import UIKit
+
+class Card: NSObject {
+
+}
