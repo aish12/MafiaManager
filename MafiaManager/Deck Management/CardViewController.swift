03862135334ba4d8e02c050c1bee61f8a6792f06
Robert Stigler <rstig@nga.cs.utexas.edu>
2019-04-05 17:41:20 -0500

    made deck editable

--- MafiaManager/Deck Management/CardViewController.swift
+++ MafiaManager/Deck Management/CardViewController.swift
@@ -22,9 +22,46 @@ class CardViewController: UIViewController {
         self.cardName.text = cardObject.value(forKey: "cardName") as? String
         self.cardDescription.text = cardObject.value(forKey: "cardDescription") as? String
         self.cardImage.image = UIImage(data: cardObject.value(forKey: "cardImage") as! Data)
+        
+        cardName.text = cardObject.value(forKey: "cardName") as? String
+        //editDeckNameTextView.textColor = UIColor.lightGray
+        cardName.becomeFirstResponder()
+        cardName.selectedTextRange = cardName.textRange(from: cardName.endOfDocument, to: cardName.endOfDocument)
+        //cardName.delegate = self
+        cardName.layer.shadowOpacity = 0.4
+        cardName.layer.shadowColor = UIColor.lightGray.cgColor
+        cardName.layer.shadowOffset = CGSize(width: 3, height: 3)
+        cardName.layer.shadowRadius = 5
+        cardName.layer.cornerRadius = 5
+        cardName.layer.masksToBounds = false
+        
+        cardDescription.text = cardObject.value(forKey: "cardDescription") as? String
+        //editDeckDescriptionTextView.textColor = UIColor.lightGray
+        //cardDescription.delegate = self
+        cardDescription.layer.shadowOpacity = 0.4
+        cardDescription.layer.shadowOffset = CGSize(width: 3, height: 3)
+        cardDescription.layer.shadowColor = UIColor.lightGray.cgColor
+        cardDescription.layer.shadowRadius = 5
+        cardDescription.layer.cornerRadius = 5
+        cardDescription.layer.masksToBounds = false
+    }
+
+    
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+        cardDescription.isEditable = true
+        cardDescription.isSelectable = true
+        cardName.isEditable = true
+        cardName.isSelectable = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(endEditState))
     }
     
-
+    @objc func endEditState() {
+        cardDescription.isEditable = false
+        cardDescription.isSelectable = false
+        cardName.isEditable = false
+        cardName.isSelectable = false
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editButtonPressed))
+    }
     /*
     // MARK: - Navigation
 
