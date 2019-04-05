3c8ed1c2c95dd81de0643aaa91dccad1a4f755b0
Tesia Wu <tesiawu@wireless-10-147-215-97.public.utexas.edu>
2019-04-05 02:36:04 -0500

    added the design to the text views in create card vc

--- MafiaManager/Deck Management/CreateCardViewController.swift
+++ MafiaManager/Deck Management/CreateCardViewController.swift
@@ -8,13 +8,39 @@
 //  Responsible for managing the card creation view controller
 
 import UIKit
+import CoreData
+import CoreGraphics
 
-class CreateCardViewController: UIViewController {
+class CreateCardViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var cardNameTextView: UITextView!
+    @IBOutlet weak var cardDescriptionTextView: UITextView!
+    @IBOutlet weak var cardImageButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        cardNameTextView.text = "Enter deck name"
+        cardNameTextView.textColor = UIColor.lightGray
+        cardNameTextView.becomeFirstResponder()
+        cardNameTextView.selectedTextRange = cardNameTextView.textRange(from: cardNameTextView.beginningOfDocument, to: cardNameTextView.beginningOfDocument)
+        cardNameTextView.delegate = self
+        cardNameTextView.layer.shadowOpacity = 0.4
+        cardNameTextView.layer.shadowColor = UIColor.lightGray.cgColor
+        cardNameTextView.layer.shadowOffset = CGSize(width: 3, height: 3)
+        cardNameTextView.layer.shadowRadius = 5
+        cardNameTextView.layer.cornerRadius = 5
+        cardNameTextView.layer.masksToBounds = false
+        
+        cardDescriptionTextView.text = "Enter deck description"
+        cardDescriptionTextView.textColor = UIColor.lightGray
+        cardDescriptionTextView.delegate = self
+        cardDescriptionTextView.layer.shadowOpacity = 0.4
+        cardDescriptionTextView.layer.shadowOffset = CGSize(width: 3, height: 3)
+        cardDescriptionTextView.layer.shadowColor = UIColor.lightGray.cgColor
+        cardDescriptionTextView.layer.shadowRadius = 5
+        cardDescriptionTextView.layer.cornerRadius = 5
+        cardDescriptionTextView.layer.masksToBounds = false
     }
     
     // When the done button is pressed, return to the previous VC,
