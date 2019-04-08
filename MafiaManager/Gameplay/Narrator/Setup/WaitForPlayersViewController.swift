e8088062078497a27d8a0067233d87efb85d9249
Tesia Wu <tesiawu@wireless-10-147-215-97.public.utexas.edu>
2019-04-08 02:13:25 -0500

    fixed networking. needs tidying but will do tomorrow good job robby. Networking is now in the vcs instead of having an object manage it

--- MafiaManager/Gameplay/Narrator/Setup/WaitForPlayersViewController.swift
+++ MafiaManager/Gameplay/Narrator/Setup/WaitForPlayersViewController.swift
@@ -5,37 +5,65 @@
 //  Created by Robert Stigler on 4/7/19.
 //  Copyright © 2019 Aishwarya Shashidhar. All rights reserved.
 //
-
 import UIKit
 import MultipeerConnectivity
 
-class WaitForPlayersViewController: UIViewController{
+class WaitForPlayersViewController: UIViewController, MCSessionDelegate {
     
-    
-    @IBOutlet weak var playerCountLabel: UILabel!
-    
-    let mpcNarrator = MPCNarrator()
     var cardQuantities: [Card: Int]?
     var connections: [Int] = []
     var numPlayers: Int = 0
+    
+    var peerID: MCPeerID!
+    var mcSession: MCSession!
+    var mcAdvertiserAssistant: MCAdvertiserAssistant!
+    var hosting: Bool!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        resetPlayerCountLabel()
-        mpcNarrator.view = self as? WaitForPlayersViewController
+        //resetPlayerCountLabel()
+        peerID = MCPeerID(displayName: UIDevice.current.name)
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession.delegate = self
+        hosting = true
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "mafiamanager-mp", discoveryInfo: nil, session: mcSession)
+        mcAdvertiserAssistant.start()
+        mcSession.disconnect()
+        
     }
     
-    func resetPlayerCountLabel(){
-        playerCountLabel.text = "\(connections.count)/\(numPlayers) Players Joined"
-
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
     }
+    
+    // MCSessionDelegate methods
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState){
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID){
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID){
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress){
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localUrl: URL?, withError error: Error?){
+        
+    }
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- MafiaManager/Gameplay/Player/JoinGameViewController.swift
+++ MafiaManager/Gameplay/Player/JoinGameViewController.swift
@@ -5,28 +5,74 @@
 //  Created by Robert Stigler on 4/7/19.
 //  Copyright © 2019 Aishwarya Shashidhar. All rights reserved.
 //
-
 import UIKit
+import MultipeerConnectivity
 
-class JoinGameViewController: UIViewController {
-
-    let mpcPlayer = MPCPlayer()
+class JoinGameViewController: UIViewController, MCSessionDelegate, MCBrowserViewControllerDelegate {
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    var peerID: MCPeerID!
+    var mcSession: MCSession!
+    var hosting: Bool!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        peerID = MCPeerID(displayName: UIDevice.current.name)
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession.delegate = self
+        hosting = false
+        
+        let mcBrowser = MCBrowserViewController(serviceType: "mafiamanager-mp", session: mcSession)
+        mcBrowser.delegate = self
+        self.present(mcBrowser, animated: true, completion: nil)
+        mcSession.disconnect()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MCSessionDelegate methods
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState){
+        
     }
-    */
-
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID){
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID){
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress){
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localUrl: URL?, withError error: Error?){
+        
+    }
+    
+    //MCBrowserViewControllerDelegate methods
+    func browserViewControllerDidFinish(){
+        
+    }
+    
+    func browserViewControllerWasCancelled(){
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
