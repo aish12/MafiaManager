72af7697c7e8f0e38b140a037c1e04a8456e1817
Tesia Wu <tesiawu@wireless-10-147-27-132.public.utexas.edu>
2019-04-09 16:07:47 -0500

    created the narrator to change stuff

---
+++ MafiaManager/Gameplay/Narrator/NarratorChangeRoleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NarratorChangeRoleViewController.swift
+//  MafiaManager
+//
+//  Created by Tesia Wu on 4/9/19.
+//  Copyright © 2019 Aishwarya Shashidhar. All rights reserved.
+//
+
+import UIKit
+
+class NarratorChangeRoleViewController: UIViewController {
+
+    @IBOutlet weak var playerImageView: UIImageView!
+    @IBOutlet weak var playerName: UITextView!
+    @IBOutlet weak var playerStatus: UILabel!
+    @IBOutlet weak var playerDescription: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
