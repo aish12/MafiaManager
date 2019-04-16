f8293332458e7b053d3f712da939b3cd24b981fa
Robert Stigler <rstig@panj.cs.utexas.edu>
2019-04-16 16:25:18 -0500

    cleaned picker code

--- MafiaManager/Gameplay/Narrator/TimerViewController.swift
+++ MafiaManager/Gameplay/Narrator/TimerViewController.swift
@@ -16,9 +16,9 @@ class TimerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     @IBOutlet weak var secondPicker: UIPickerView!
     @IBOutlet weak var countdownLabel: UILabel!
     
+    let maxMinuteValue: Int = 15
+    let maxSecondValue: Int = 59
     var timer: GlobalTimer = GlobalTimer.sharedTimer
-    let minutePickerValues: [Int] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-    let secondPickerValues: [Int] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -85,9 +85,9 @@ class TimerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     // Determine which picker is being used, then return the count of its data source
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if pickerView.isEqual(minutePicker){
-            return minutePickerValues.count
+            return maxMinuteValue + 1
         } else if pickerView.isEqual(secondPicker){
-            return secondPickerValues.count
+            return maxSecondValue + 1
         } else {
             print("Failed Picker")
             return 0
@@ -96,13 +96,6 @@ class TimerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     
     // Determine which picker is being used, then return the value at the given index/row
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView.isEqual(minutePicker){
-            return "\(minutePickerValues[row])"
-        } else if pickerView.isEqual(secondPicker){
-            return "\(secondPickerValues[row])"
-        } else {
-            print("Failed Picker")
-            return ""
-        }
+        return "\(row)"
     }
 }
